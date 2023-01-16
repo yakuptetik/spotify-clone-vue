333472ab0ca8ab1cc03730ba119d7ee6e1afc90a
Yakup <yakuptetik13@gmail.com>
2023-01-16 08:23:26 +0300

    sd

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,7 +13,7 @@
        <SearchItem bgorange songImage="https://i.scdn.co/image/ab67706f000000029bb6af539d072de34548d15c" tittles="Pop"/>
        <SearchItem bgpink songImage="https://i.scdn.co/image/ab67706f000000029bb6af539d072de34548d15c" tittles="Pop"/>
       </CategoryItem>
-
+df
 
     </div>
 </template>
